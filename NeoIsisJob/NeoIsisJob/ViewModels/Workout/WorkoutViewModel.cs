8168c01f08675c68cdd78cc57798ab493d2594cf
George Roman <roman.geo@yahoo.com>
2025-03-31 22:08:09 +0300

    complete filter for workout page

--- NeoIsisJob/NeoIsisJob/ViewModels/Workout/WorkoutViewModel.cs
+++ NeoIsisJob/NeoIsisJob/ViewModels/Workout/WorkoutViewModel.cs
@@ -6,6 +6,8 @@ using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using Microsoft.UI.Xaml;
 using NeoIsisJob.Commands;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace NeoIsisJob.ViewModels.Workout
 {
@@ -13,7 +15,10 @@ namespace NeoIsisJob.ViewModels.Workout
     public class WorkoutViewModel : INotifyPropertyChanged
     {
         private readonly WorkoutService _workoutService;
+        private readonly WorkoutTypeService _workoutTypeService;
         private ObservableCollection<WorkoutModel> _workouts;
+        private ObservableCollection<WorkoutTypeModel> _workoutTypes;
+        private WorkoutTypeModel _selectedWorkoutType;
 
         //Workouts property for the list -> when set it, signal that it changed
         public ObservableCollection<WorkoutModel> Workouts
@@ -26,18 +31,46 @@ namespace NeoIsisJob.ViewModels.Workout
             }
         }
 
+        public ObservableCollection<WorkoutTypeModel> WorkoutTypes
+        {
+            get => _workoutTypes;
+            set
+            {
+                _workoutTypes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public WorkoutTypeModel SelectedWorkoutType
+        {
+            get => _selectedWorkoutType;
+            set
+            {
+                //when setting, call the function too to update the displayed list
+                _selectedWorkoutType = value;
+                OnPropertyChanged();
+                //apply the filter when changed
+                ApplyWorkoutFilter();
+            }
+        }
+
         public ICommand LoadWorkoutsCommand { get; }
+
+        public ICommand LoadWorkoutTypesCommand { get; }
 
         public WorkoutViewModel()
         {
             this._workoutService = new WorkoutService();
+            this._workoutTypeService = new WorkoutTypeService();
             Workouts = new ObservableCollection<WorkoutModel>();
+            WorkoutTypes = new ObservableCollection<WorkoutTypeModel>();
 
-            // Command to load workouts
+            // Command to load workouts --- FOR NOW IT IS COMMENTED!
             //LoadWorkoutsCommand = new RelayCommand(LoadWorkouts);
 
             // Load workouts when viewmodel is created
             LoadWorkouts();
+            LoadWorkoutTypes();
         }
 
         private void LoadWorkouts()
@@ -52,6 +85,39 @@ namespace NeoIsisJob.ViewModels.Workout
             foreach (var workout in this._workoutService.GetAllWorkouts())
             {
                 Workouts.Add(workout);
+            }
+        }
+
+        private void LoadWorkoutTypes()
+        {
+            WorkoutTypes.Clear();
+            foreach (var workoutType in this._workoutTypeService.GetAllWorkoutTypes())
+            {
+                WorkoutTypes.Add(workoutType);
+            }
+        }
+
+        private void ApplyWorkoutFilter()
+        {
+            Workouts.Clear();
+            IList<WorkoutModel> allWorkouts = this._workoutService.GetAllWorkouts();
+
+            //if a checkbox is selected
+            if(SelectedWorkoutType != null)
+            {
+                //filter the list
+                foreach(WorkoutModel workout in allWorkouts.Where(w => w.WorkoutTypeId == SelectedWorkoutType.Id))
+                {
+                    Workouts.Add(workout);
+                }
+            }
+            //if not
+            else
+            {
+                foreach(WorkoutModel workout in allWorkouts)
+                {
+                    Workouts.Add(workout);
+                }
             }
         }
 
