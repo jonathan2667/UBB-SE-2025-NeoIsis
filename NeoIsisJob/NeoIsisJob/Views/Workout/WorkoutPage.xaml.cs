47f24eadd05eefa64649a0af44c8736891b7c7cf
George Roman <roman.geo@yahoo.com>
2025-03-31 22:32:35 +0300

    filter

--- NeoIsisJob/NeoIsisJob/Views/Workout/WorkoutPage.xaml.cs
+++ NeoIsisJob/NeoIsisJob/Views/Workout/WorkoutPage.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using NeoIsisJob.ViewModels.Workout;
+using NeoIsisJob.Models;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -59,6 +60,69 @@ namespace NeoIsisJob.Views
         public void GoToRankingPage_Tap(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(RankingPage));
+        }
+
+        //handler for checking a box
+        private void WorkoutTypeCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            //if the sender is a checkbox and its data is a workout type
+            if (sender is CheckBox checkBox && checkBox.DataContext is WorkoutTypeModel selectedType)
+            {
+                //var viewModel = DataContext as WorkoutViewModel;
+                if (this.ViewModel == null) return;
+
+                //if checked, filter workouts and disable other checkboxes
+                if (checkBox.IsChecked == true)
+                {
+                    //this triggers the list of workouts to change
+                    this.ViewModel.SelectedWorkoutType = selectedType;
+                    DisableOtherCheckBoxes(selectedType);
+                }
+                else
+                {
+                    //reset the filter
+                    this.ViewModel.SelectedWorkoutType = null;
+                    EnableAllCheckBoxes();
+                }
+            }
+        }
+
+        //disable all checkboxes except the selected one
+        private void DisableOtherCheckBoxes(WorkoutTypeModel selectedType)
+        {
+            foreach (CheckBox checkBox in FindVisualChildren<CheckBox>(this))
+            {
+                if (checkBox.DataContext is WorkoutTypeModel type && type != selectedType)
+                {
+                    checkBox.IsEnabled = false;
+                }
+            }
+        }
+
+        //re-enable all checkboxes when filter is removed
+        private void EnableAllCheckBoxes()
+        {
+            foreach (CheckBox checkBox in FindVisualChildren<CheckBox>(this))
+            {
+                checkBox.IsEnabled = true;
+            }
+        }
+
+        //finds all the checkboxes in the visual tree
+        private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield break;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                var child = VisualTreeHelper.GetChild(depObj, i);
+                if (child is T childItem) yield return childItem;
+
+                foreach (var childOfChild in FindVisualChildren<T>(child))
+                {
+                    yield return childOfChild;
+                }
+            }
         }
     }
 }
