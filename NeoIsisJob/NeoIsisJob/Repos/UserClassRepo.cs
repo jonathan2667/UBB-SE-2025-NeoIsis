ff5370a417f25889db0f06c3e5107fb4bfc8df40
916-Racsan-Rares <rares.racsan@gmail.com>
2025-04-02 09:30:43 +0300

    added getByDate for userworkouts and userClasses

--- NeoIsisJob/NeoIsisJob/Repos/UserClassRepo.cs
+++ NeoIsisJob/NeoIsisJob/Repos/UserClassRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -107,28 +108,61 @@ namespace NeoIsisJob.Repos
                 command.ExecuteNonQuery();
             }
         }
-    
+
         public void DeleteUserClassModel(int userId, int classId, DateTime enrollmentDate)
         {
             using (SqlConnection connection = _dbHelper.GetConnection())
             {
                 // Open the connection
                 connection.Open();
-                
+
                 // Create a query
                 string query = "DELETE FROM UserClasses WHERE UID = @UID AND CID = @CID AND Date = @Date";
-                
+
                 // Create a command
                 SqlCommand command = new SqlCommand(query, connection);
-                
+
                 // Add the parameters
                 command.Parameters.AddWithValue("@UID", userId);
                 command.Parameters.AddWithValue("@CID", classId);
                 command.Parameters.AddWithValue("@Date", enrollmentDate);
-                
+
                 // Execute the command
                 command.ExecuteNonQuery();
             }
+        }
+
+        public List<UserClassModel> GetUserClassModelByDate(DateTime date)
+        {
+            List<UserClassModel> userClasses = new List<UserClassModel>();
+            using (SqlConnection connection = _dbHelper.GetConnection())
+            {
+                // Open the connection
+                connection.Open();
+
+                // Create a query
+                string query = "SELECT UID, CID, Date FROM UserClasses WHERE Date = @Date";
+
+                // Create a command
+                SqlCommand cmd = new SqlCommand(query, connection);
+
+                // Add the parameters
+                cmd.Parameters.AddWithValue("@Date", date);
+
+                // Execute the command
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                // Read the data
+                while (reader.Read())
+                {
+                    userClasses.Add(new UserClassModel(
+                        (int)reader["UID"],
+                        (int)reader["CID"],
+                        (DateTime)reader["Date"]
+                    ));
+                }
+            }
+            return userClasses;
         }
     }
 }
