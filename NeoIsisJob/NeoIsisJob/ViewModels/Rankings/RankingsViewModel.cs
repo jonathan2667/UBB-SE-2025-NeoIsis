5e222cccf15ba528c5346419e24e3ba648031ce7
916 - Popa-Zelu-Andrei <applekiller733@gmail.com>
2025-04-03 13:49:33 +0300

    Rankings page revised, empty Viewmodel created

---
+++ NeoIsisJob/NeoIsisJob/ViewModels/Rankings/RankingsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NeoIsisJob.Models;
+using NeoIsisJob.Servs;
+using NeoIsisJob.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoIsisJob.Commands;
+
+namespace NeoIsisJob.ViewModels.Rankings
+{
+    class RankingsViewModel
+    {
+        
+    }
+}
