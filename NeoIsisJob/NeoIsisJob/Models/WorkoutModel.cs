ad72f59928936747eab9de78edc2fa34c4bd7f6d
gloriapascuti <108720092+gloriapascuti@users.noreply.github.com>
2025-04-14 18:29:50 +0300

    Logic slayer paladin (#2)
    * Fix calendar business logic in GUI
    
    Added a service interface and implementation to handle calendar-related functionality.
    
    * Modified calendar view model file
    
    * Moved business logic from GUI for all classes-related functions.
    Added an interface for the class service
    
    * Adressed review comments: changed the Servs file into Services
    
    * Made more changes so that everything runs smoothly.
    
    * Refactor CalendarService to delegate SQL operations to repository
    
    ---------
    
    Co-authored-by: GLORIA-ANAMARIA PĂȘCUȚI <gloria.pascuti@stud.ubbcluj.ro>

--- NeoIsisJob/NeoIsisJob/Models/WorkoutModel.cs
+++ NeoIsisJob/NeoIsisJob/Models/WorkoutModel.cs
@@ -5,18 +5,21 @@
         private int _id;
         private string _name;
         private int _workoutTypeId;
+        private string _description;
 
         public int Id { get => _id; set => _id = value; }
         public string Name { get => _name; set => _name = value; }
         public int WorkoutTypeId { get => _workoutTypeId; set => _workoutTypeId = value; }
+        public string Description { get => _description; set => _description = value; }
 
         public WorkoutModel() { }
 
-        public WorkoutModel(int id, string name, int workoutTypeId)
+        public WorkoutModel(int id, string name, int workoutTypeId, string description = null)
         {
             Id = id;
             Name = name;
             WorkoutTypeId = workoutTypeId;
+            Description = description;
         }
     }
 }
---
+++ NeoIsisJob/NeoIsisJob/ViewModels/ClassesViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using NeoIsisJob.Servs;
+
+namespace NeoIsisJob.ViewModels
+{
+    public class ClassesViewModel
+    {
+        private readonly IClassService _classService;
+
+        public ClassesViewModel(IClassService classService)
+        {
+            _classService = classService;
+        }
+
+        public string ConfirmRegistration(int userId, int classId, DateTime date)
+        {
+            return _classService.ConfirmRegistration(userId, classId, date);
+        }
+    }
+} 
