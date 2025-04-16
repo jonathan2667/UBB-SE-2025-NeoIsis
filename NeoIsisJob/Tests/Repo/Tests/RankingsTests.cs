7ba84a31e756844876db8cdced398c60761ebf24
915-Nicola-Razvan-Cristian <razvannicola451@gmail.com>
2025-04-16 11:02:28 +0300

    mocks + tests folder

---
+++ NeoIsisJob/Tests/Repo/Tests/RankingsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Repo.Mocks;
+
+namespace Tests.Repo.Tests
+{
+    [TestClass]
+    class RankingsTests
+    {
+        private readonly RankingsMock _rankingRepository = new RankingsMock();
+
+        [TestMethod]
+        public void TestGetByUserID()
+        {
+            Assert.Equals(_rankingRepository.GetAllRankingsByUserID(1)
+        }
+    }
+}
--- NeoIsisJob/Tests/Test1.cs
+++ NeoIsisJob/Tests/Test1.cs
@@ -4,6 +4,7 @@ namespace Tests
     [TestClass]
     public sealed class Test1
     {
+
         [TestMethod]
         public void TestMethod1()
         {
