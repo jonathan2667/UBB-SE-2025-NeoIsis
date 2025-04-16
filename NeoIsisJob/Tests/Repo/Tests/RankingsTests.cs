01dcc41ea6d1698bb1477b0b335aea8c4da0ac8a
ElizaLuchian <56509779+ElizaLuchian@users.noreply.github.com>
2025-04-16 20:31:51 +0300

    Unit testing ninja (#5)
    * Light mode
    
    * Revert "Light mode"
    
    This reverts commit c27aa42c10e552ba44edcf2dff0db73eba9fa0db.
    
    * Light mode
    
    * Created MSTest project for unit testing
    
    * mocks + tests folder
    
    * fix
    
    * modifications from my project
    
    ---------
    
    Co-authored-by: 915-Nicola-Razvan-Cristian <razvannicola451@gmail.com>

---
+++ NeoIsisJob/Tests/Repo/Tests/RankingsTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Repo.Mocks;
+
+namespace Tests.Repo.Tests
+{
+    [TestClass]
+    class RankingsTests
+    {
+        private readonly RankingsMock _rankingRepository = new RankingsMock();
+
+        [TestMethod]
+        public void TestGetByUserID()
+        {
+        }
+    }
+}
---
+++ NeoIsisJob/Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var workoutModel = new NeoIsisJob.Models.WorkoutModel(1, "Test Workout", 1, "Test Description");
+            // Act
+            var id = workoutModel.Id;
+            var name = workoutModel.Name;
+            var workoutTypeId = workoutModel.WorkoutTypeId;
+            var description = workoutModel.Description;
+            // Assert
+            Assert.AreEqual(1, id);
+            Assert.AreEqual("Test Workout", name);
+            Assert.AreEqual(1, workoutTypeId);
+            Assert.AreEqual("Test Description", description);
+        }
+    }
+}
